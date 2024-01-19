272d4d75b0130120dad95c4a19bfbe266b7adb9f
Rahul Patel <rspatel031@gmail.com>
2024-01-19 16:15:33 +0530

    Initial Project Setup

---
+++ src/test/java/com/rahulpateldev/springframeworkv6/SpringFrameworkV6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulpateldev.springframeworkv6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkV6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
